417e100d06a4d252e1397c11fa6cede026f89131
Stephen Rodrigues Knupfer <stephenrknupfer@hotmail.com>
2025-10-17 14:55:36 -0300

    expo-router configuration

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text className="text-3xl text-blue-500 font-bold">Home</Text>
+    </View>
+  );
+}
